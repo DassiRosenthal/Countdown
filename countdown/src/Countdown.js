ca18d81b5c705bac18d0b58b751014b02ddb02f0
Dassi Rosenthal <dassirosenthal@gmail.com>
2024-09-26 21:47:47 -0400

    fixes

--- countdown/src/Countdown.js
+++ countdown/src/Countdown.js
@@ -7,7 +7,14 @@ export default function Countdown(props) {
     const [timeLeft, setTimeLeft] = useState(new Date('December 5, 2024 06:00:00') - new Date());
     const [timeDisplay, setTimeDisplay] = useState(1);
     const [page, setPage] = useState(0);
+    const [displayCover, setDisplayCover] = useState(true);
 
+
+    useEffect(() => {
+        setTimeout(() => {
+            setDisplayCover(false);
+        }, 1000)
+    }, [])
 
     useEffect(() => {
         setTimeout(() => {
@@ -16,55 +23,58 @@ export default function Countdown(props) {
     }, [timeLeft])
 
     useEffect(() => {
-        setTimeout(() => {
-            if (page < images1.length - 1) {
-                setPage(page + 1)
-            }
-            else {
-                setPage(0)
-            }
-        }, 6000)
-    }, [page])
+        if (!displayCover) {
+            setTimeout(() => {
+                if (page < images1.length - 1) {
+                    setPage(page + 1)
+                }
+                else {
+                    setPage(0)
+                }
+            }, 6000)
+        }
+    }, [page, displayCover, images1.length])
 
     useEffect(() => {
-        setTimeout(() => {
-            if (timeDisplay < 6) {
-                setTimeDisplay(timeDisplay + 1)
-            }
-            else {
-                setTimeDisplay(1)
-            }
-        }, 4000)
-    }, [timeDisplay])
+        if (!displayCover) {
+            setTimeout(() => {
+                if (timeDisplay < 5) {
+                    setTimeDisplay(timeDisplay + 1)
+                }
+                else {
+                    setTimeDisplay(1)
+                }
+            }, 4000)
+        }
+    }, [timeDisplay, displayCover])
 
 
     return (
         <div className={`outer-div`}>
-            <div className='time-left-div'>
-                {timeDisplay === 1 ?
-                    <div className='mazel-tov'>MAZEL TOV!</div>
-                    : timeDisplay === 2 ?
+            {displayCover ? <div><div className='mazel-tov'>MAZEL TOV!</div></div>
+                : <><div className='time-left-div'>
+                    {timeDisplay === 1 ?
                         <div>{Math.round(timeLeft / 86400000).toLocaleString()} days</div>
-                        : timeDisplay === 3 ?
+                        : timeDisplay === 2 ?
                             <div>{Math.round(timeLeft / 3600000).toLocaleString()} hours</div>
-                            : timeDisplay === 4 ?
+                            : timeDisplay === 3 ?
                                 <div>{Math.round(timeLeft / 60000).toLocaleString()} minutes</div>
-                                : timeDisplay === 5 ?
+                                : timeDisplay === 4 ?
                                     <div>{Math.round(timeLeft / 1000).toLocaleString()} seconds</div>
                                     : <div className='cant-wait'>Can't wait!</div>}
 
-            </div>
+                </div>
 
-            <div>{images1.map((img, index) =>
-                <img src={img} alt='background'
-                    className={`image-1 ${page === index ? 'background-image' : (page - 1 === index || (index === images1.length - 1 && page === 0)) ? 'leave' : 'hide'}`}
-                />
-            )}</div>
-            <div> {images2.map((img, index) =>
-                <img src={img} alt='background'
-                    className={`image-2 ${page === index ? 'background-image' : (page - 1 === index || (index === images1.length - 1 && page === 0)) ? 'leave' : 'hide'}`}
-                />
-            )}</div>
+                    <div>{images1.map((img, index) =>
+                        <img src={img} alt='background'
+                            className={`image-1 ${page === index ? 'background-image' : (page - 1 === index || (index === images1.length - 1 && page === 0)) ? 'leave' : 'hide'}`}
+                        />
+                    )}</div>
+                    <div> {images2.map((img, index) =>
+                        <img src={img} alt='background'
+                            className={`image-2 ${page === index ? 'background-image' : (page - 1 === index || (index === images1.length - 1 && page === 0)) ? 'leave' : 'hide'}`}
+                        />
+                    )}</div></>}
         </div>
     )
 }
